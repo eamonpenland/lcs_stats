1be2cfd001b52961e0f4b1f1ac41f8daac28437f
Jeff Gandt <jeff.gandt@gmail.com>
2017-06-24 22:18:39 -0400

    bug fix

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LcsStats.Mixfile do
 
   def project do
     [app: :lcs_stats,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
